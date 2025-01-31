37386b34468b3ff0b6b5af3babb7c23effd78228
Jose Chiluiza <jose_newboy@hotmail.com>
2025-01-31 17:31:28 -0500

    Add files via upload

---
+++ Si_puede_votar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduce tu edad: ");
+        int edad = int.Parse(Console.ReadLine());
+
+        if (edad >= 18)
+        {
+            Console.WriteLine("Eres elegible para votar.");
+        }
+        else
+        {
+            Console.WriteLine("No puedes votar, necesitas ser mayor de 18 años.");
+        }
+    }
+}
